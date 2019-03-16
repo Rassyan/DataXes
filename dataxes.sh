11b6ae34237e5b729ff92184635231f31d025faf
Rassyan <yjkhngds@gmail.com>
2019-03-16 20:42:02 +0800

    用于docker的简化一键命令

---
+++ dataxes.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+if [ -z "$1" ]; then
+    echo "No job name parameter, run JDBC Job Tool."
+    docker run --rm -it -v $(pwd):/opt/dataxes/ rassyan/dataxes python /opt/datax/bin/jdbc_job_tool.py
+    exit 0
+fi
+
+suffix=`date "+%Y%m%d%H%M%S"`
+dataxes_job_name=$1
+dataxes_args=$2
+if [ "-d" == "$1" ]; then
+    docker_args=$1
+    dataxes_job_name=$2
+    dataxes_args=$3
+fi
+
+docker run --rm ${docker_args} \
+-v $(pwd):/opt/dataxes/ \
+--name dataxes_${dataxes_job_name//@/_}_${suffix} \
+rassyan/dataxes \
+python -u ${dataxes_job_name}.py ${dataxes_args}
